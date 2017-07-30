12876e821555ad1d78cbb7634580179c43e65cd3
Alexander Cobleigh <tna11aco@student.lu.se>
2017-07-30 22:14:15 +0200

    looks like it's working?

--- remember.py
+++ remember.py
@@ -3,7 +3,10 @@
 import datetime
 import io
 import json
-from collections import namedtuple
+
+DATE_FORMAT = "%Y-%m-%d"
+TODAY = datetime.datetime.today().strftime(DATE_FORMAT)
+
 
 # INTENT
 # i want to remember specific items using prompts to aid recall like
@@ -17,16 +20,17 @@ from collections import namedtuple
 #   a creation date (unixtime), 
 #   a review date 
 #   and a stage
-Task = namedtuple("Task", ["date", "stage", "id", "creation_date"])
+
+
 
 # afterwards each stage's interval is calculated as the sum of the two preceeding intervals
 # next_interval = schedule[stage-1] + schedule[stage-2]
 # so interval = schedule[3] + schedule[4] = 6 + 12 = 18
-for a in xrange(10):
-    interval = schedule[-1] + schedule[-2]
-    schedule.append(interval)
-
-print schedule
+# for a in xrange(10):
+#     interval = schedule[-1] + schedule[-2]
+#     schedule.append(interval)
+#
+# print schedule
 
 # interval => min(schedule[stage], 365*3)
 
@@ -35,97 +39,125 @@ print schedule
 #   a counter (from which to spawn ids)
 #   the review schedule (a list of intervals)
 #   a map of tasks (map[task.id] => task)
-class Database:
-    # a counter (from which to spawn ids)
-    self.counter = 0
-    # the map of tasks i currently want to remember
-    self.tasks = dict()
-    # populated from self.tasks with dates which contain the lists of tasks for review
-    self.calendar = dict()
-    # list of intervals, each index correlating to the stage a task may be in
-    self.schedule = [1, 2, 6, 12, 18, 30, 48, 78, 126, 204, 330, 534, 864, 1398]
-    # the list of tasks for review today
-    self.todays_tasks = list()
-
-    # max amount of tasks to review each day
-    self.MAX_TASKS = 4
-    # 4 years between reviews is like... enough. i can take time out to review
-    # well-known concepts every 4 years
-    self.MAX_REVIEW_INTERVAL = 365 * 4
-    # path where flatfile database resides
-    self.DEFAULT_PATH = "./srs.json"
+class Database(object):
 
     def __init__(self):
+        # a counter (from which to spawn ids)
+        self.counter = 0
+        # the map of tasks i currently want to remember
+        self.tasks = dict()
+        # populated from self.tasks with dates which contain the lists of tasks for review
+        self.calendar = dict()
+        # list of intervals, each index correlating to the stage a task may be in
+        self.schedule = [1, 2, 6, 12, 18, 30, 48, 78, 126, 204, 330, 534, 864, 1398]
+        # the list of tasks for review today
+        self.todays_tasks = list()
+        # max amount of tasks to review each day
+        self.MAX_TASKS = 4
+        # 4 years between reviews is like... enough. i can take time out to review
+        # well-known concepts every 4 years
+        self.MAX_REVIEW_INTERVAL = 365 * 4
+        # path where flatfile database resides
+        self.DEFAULT_PATH = "srs.json"
+
         self.load()
-        self.populate_calendar()
+        self.calendar = self.populate_calendar()
+        self.todays_tasks = []
+        if TODAY in self.calendar:
+            self.todays_tasks = self.calendar[TODAY]
 
     def generate_id(self):
         task_id = self.counter
         # increment counter
         self.counter += 1
+        self.save()
         return task_id
 
     def load(self):
-        with open(self.DEFAULT_PATH, "r") as f:
-            obj = json.load(f)
-            # these wont exist on first load
-            if "tasks" in obj:
-                self.tasks = {task["id"]: Task(**task) for (_, task) in obj["tasks"]}
-            if "counter" in obj:
-                self.counter = obj["counter"]
+        try: 
+            with io.open(self.DEFAULT_PATH, "r", encoding="utf-8") as f:
+                obj = json.load(f)
+                print obj
+                # these wont exist on first load
+                if "tasks" in obj:
+                    self.tasks = obj["tasks"]
+                if "counter" in obj:
+                    self.counter = obj["counter"]
+        except IOError as e:
+            print e
 
     def save(self):
-        with io.open(DEFAULT_PATH, "w", encoding="utf8") as f:
-            f.write(json.dumps({"tasks": self.tasks, "counter": self.counter}, ensure_ascii=False))
+        with io.open(self.DEFAULT_PATH, "w", encoding="utf-8") as f:
+            data = json.dumps({"tasks": self.tasks, "counter": self.counter}, ensure_ascii=False)
+            if isinstance(data, str):
+                data = data.decode("utf-8")
+            f.write(data)
 
     def populate_calendar(self):
         calendar = dict()
-        for task in self.tasks:
-            date = task.date
+        for _, task in self.tasks.items():
+            print task
+            date = task["review_date"]
             if not date in calendar:
                 calendar[date] = []
             calendar[date].append(task)
         return calendar
 
-    def check_calendar(task):
-        calendar_tasks = calendar[task.date]
+    def check_calendar(self, task):
+        calendar_tasks = calendar[task["review_date"]]
         if len(calendar_tasks) > MAX_TASKS:
             # schedule it a day earlier
-            task.date = task.date - 1
+            task["review_date"] = task["review_date"] - 1
         else:
-            return task.date
+            return task["review_date"]
         return self.check_calendar(task)
 
-    def get_interval(task):
+    def get_interval(self, task):
         # this is a very mature task, set its review time to the max
-        if task.stage > len(schedule):
+        if task["stage"] > len(self.schedule):
             return MAX_REVIEW_INTERVAL
-        return schedule[task.stage]
+        return self.schedule[task["stage"]]
 
-    def schedule_task(task):
-        interval = get_interval(task)
-        task.date += interval
-        update_calendar(task)
+    def schedule_task(self, task):
+        interval = datetime.timedelta(days=self.get_interval(task))
+        new_date = datetime.datetime.strptime(task["review_date"], DATE_FORMAT) + interval
+        task["review_date"] = new_date.strftime(DATE_FORMAT)
+        self.update_calendar(task)
+        self.save()
 
-    def update_calendar(task):
-        database["tasks"][task.id] = task
+    def update_calendar(self, task):
+        self.tasks[task["task_id"]] = task
+        self.save()
 
-def remember(task):
-    init()
-    task.stage = 0
-    task.id = generate_id()
-    schedule_task(task)
-    save(database)
+    def remember(self, description):
+        task = dict()
+        task["data"] = description
+        task["stage"] = 0
+        task["review_date"] = TODAY
+        task["creation_date"] = TODAY
+        task["task_id"] = self.generate_id()
+        self.schedule_task(task)
+        self.save()
+
+    def review(self, task_number, grade):
+        task = self.todays_tasks[task_number]
+        # maybe grade is how to increment the stage
+        # 0 = reset, 1 = next stage, 2 = two stages forward, -1 = previous stage
+        if grade == 0:
+            task["stage"] = 0
+        else:
+            task["stage"] += grade
+        self.schedule_task(task)
+        self.save()
+        # return amount of days before next review
+        return self.schedule[task.stage]
+
+def remember(description):
+    database.remember(description)
 
 def review(task, grade):
-    init()
-    # maybe grade is how to increment the stage
-    # 0 = reset, 1 = next stage, 2 = two stages forward, -1 = previous stage
-    if grade == 0:
-        task.stage = 0
-    else:
-        task.stage += grade
-    schedule_task(task)
-    save(database)
-    # return amount of days before next review
-    return schedule[task.stage]
+    return database.review(task, grade)
+
+d = Database()
+d.remember("first item")
+
